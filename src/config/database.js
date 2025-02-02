599eb16911626c7debc37fbbaba53b64d6758b2f
Abdul Mannan <itsmannan56@gmail.com>
2025-02-02 23:59:48 +0500

    Big Fixed

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres',
     logging: false,
     timezone: '+00:00',
     keepAlive: true,
@@ -16,7 +16,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
         idle: 10000,
     }
 });
-
+console.log(process.env.DB_DIALECT,)
 sequelize.authenticate()
     .then(() => {
         console.log('Connection has been established successfully.');
